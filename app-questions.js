6c9071cf3a9ea2668ea191decabaec058ad3083f
GrRakshith <rrakshithgr@outlook.com>
2018-11-25 20:35:29 +0530

    first commit

---
+++ app-questions.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        "q": "Whom you want to be class representative?",
+        "a": "shreyas",
+        "b": "samay",
+        "c": "preksha",
+        "d": "no one"
+    },
+    {
+        "q": "Who is the CEO of Apple?",
+        "a": "Tim Cook",
+        "b": "Jerry Scott",
+        "c": "Michael Dell",
+        "d": "Stephen Curry"
+    },
+    {
+        "q": "Who is the CEO of Tesla?",
+        "a": "Nikola Tesla",
+        "b": "Elon Musk",
+        "c": "Nicki Minaj",
+        "d": "Michael Jordan"
+    }
+    
+];
